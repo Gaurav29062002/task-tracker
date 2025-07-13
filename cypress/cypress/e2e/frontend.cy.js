61a8957cec647dc67e9931c214a8fb09c604394d
Gaurav29062002 <gauravsanjaysonar81@gmail.com>
2025-07-13 23:04:04 +0530

    added

--- cypress/cypress/e2e/frontend.cy.js
+++ cypress/cypress/e2e/frontend.cy.js
@@ -1,11 +1,13 @@
+const { it } = require("node:test");
+
 describe('Task Tracker Frontend', () => {
     it('loads the page and adds a task', () => {
-      cy.visit('http://frontend') // using service name in Docker
+      cy.visit('http://frontend');
   
-      cy.get('input#title').type('Test Task')
-      cy.get('button[type="submit"]').click()
-  
-      cy.get('ul#task-list li').should('contain', 'Test Task')
-    })
-  })
+      cy.get('input#title').type('Test Task');
+      cy.get('button[type="submit"]').click();
+      cy.get('#task-list li', { timeout: 10000 }).should('contain.text', 'Test Task');
+
+    });
+  });
   
